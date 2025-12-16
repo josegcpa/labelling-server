ecce1926e0ff424d03289648b2957480764151fe
josegcpa <jose.almeida@research.fchampalimaud.org>
2025-12-16 17:00:22 +0000

    more generic label_hierarchy specification

--- project/classification_hierarchies.py
+++ project/classification_hierarchies.py
@@ -1,11 +1,12 @@
-label_hierarchy = [
-    {'is_group':True,
-     'name':"Animals",
-     'elements':{"dog":"Dog","cat":"Cat"}},
-    {'is_group':False,
-     'name':"",
-     'elements':{"per":"Person"}}]
-labels_dict = {}
-for k in label_hierarchy:
-    for kk in k['elements']:
-        labels_dict[kk] = k['elements'][kk]
+import yaml
+
+def get_label_hierarchy_dict(config_path: str):
+    with open(config_path) as o:
+        label_hierarchy = yaml.safe_load(o)["classes"]
+    labels_dict = {}
+    for k in label_hierarchy:
+        for kk in k['elements']:
+            labels_dict[kk] = k['elements'][kk]
+    return label_hierarchy, labels_dict
+
+label_hierarchy, labels_dict = get_label_hierarchy_dict("config.yaml")
